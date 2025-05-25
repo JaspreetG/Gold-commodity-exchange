60412e75966494f0e5d8c8a26506d16fa7d90b90
JaspreetG <99363580+JaspreetG@users.noreply.github.com>
2025-05-26 02:07:20 +0530

    c++ matcher v1.01

---
+++ matcher/include/core/IMatchingStrategy.hpp
@@ -0,0 +1,18 @@
+#pragma once
+#include <vector>
+#include "models/Trade.hpp"
+#include "core/Order.hpp"
+#include "core/OrderBook.hpp"
+
+namespace core
+{
+
+    class IMatchingStrategy
+    {
+    public:
+        virtual ~IMatchingStrategy() = default;
+        virtual std::vector<models::Trade>
+        match(core::Order &incoming, core::OrderBook &book) = 0;
+    };
+
+} // namespace core
