c5f088003e438919d0ee591b5af4a4e61d32e7d9
JaspreetG <99363580+JaspreetG@users.noreply.github.com>
2025-05-28 18:36:50 +0530

    fixed orderbook snapshot

--- matcher/src/models/OrderBookSnapshot.cpp
+++ matcher/src/models/OrderBookSnapshot.cpp
@@ -9,11 +9,37 @@ namespace models
         std::chrono::system_clock::time_point ts)
         : bids_(bids), asks_(asks), ts_(ts) {}
 
-    const std::map<double, std::list<core::Order>, std::greater<>> &OrderBookSnapshot::bids() const { return bids_; }
-    const std::map<double, std::list<core::Order>> &OrderBookSnapshot::asks() const { return asks_; }
     std::chrono::system_clock::time_point OrderBookSnapshot::timestamp() const
     {
         return ts_;
+    }
+    std::map<double, double, std::greater<>> OrderBookSnapshot::bidVolumes() const
+    {
+        std::map<double, double, std::greater<>> result;
+        for (std::map<double, std::list<core::Order>, std::greater<>>::const_iterator it = bids_.begin(); it != bids_.end(); ++it)
+        {
+            double volume = 0.0;
+            for (std::list<core::Order>::const_iterator orderIt = it->second.begin(); orderIt != it->second.end(); ++orderIt)
+            {
+                volume += orderIt->quantity();
+            }
+            result[it->first] = volume;
+        }
+        return result;
+    }
+    std::map<double, double> OrderBookSnapshot::askVolumes() const
+    {
+        std::map<double, double> result;
+        for (std::map<double, std::list<core::Order>>::const_iterator it = asks_.begin(); it != asks_.end(); ++it)
+        {
+            double volume = 0.0;
+            for (std::list<core::Order>::const_iterator orderIt = it->second.begin(); orderIt != it->second.end(); ++orderIt)
+            {
+                volume += orderIt->quantity();
+            }
+            result[it->first] = volume;
+        }
+        return result;
     }
 
 } // namespace models
