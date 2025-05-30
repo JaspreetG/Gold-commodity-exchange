c58da5480e908fc8039e597ed1017678a424d071
rahul <rahuljha48997@gmail.com>
2025-05-31 04:10:21 +0530

    auth-microservice-testing_done

---
+++ backend/auth-service/src/main/java/io/goldexchange/auth_service/dto/LoginRequest.java
@@ -0,0 +1,11 @@
+package io.goldexchange.auth_service.dto;
+
+public class LoginRequest {
+    private String phoneNumber;
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    } 
+
+
+}
