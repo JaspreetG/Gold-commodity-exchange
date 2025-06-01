d9e120ab5090f0433d9f478e49f56d3ab3cf4d6b
rahul <rahuljha48997@gmail.com>
2025-06-01 20:48:57 +0530

    kafka configuration done in trade micoservice

--- backend/trade-service/src/main/java/io/goldexchange/trade_service/consumer/OrderBookConsumer.java
+++ backend/trade-service/src/main/java/io/goldexchange/trade_service/consumer/OrderBookConsumer.java
@@ -1,5 +1,20 @@
 package io.goldexchange.trade_service.consumer;
 
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.messaging.simp.SimpMessagingTemplate;
+import org.springframework.stereotype.Component;
+
+@Component
 public class OrderBookConsumer {
-    
+    private final SimpMessagingTemplate messagingTemplate;
+
+    public OrderBookConsumer(SimpMessagingTemplate messagingTemplate) {
+        this.messagingTemplate = messagingTemplate;
+    }
+
+    @KafkaListener(topics = "orderbook", groupId = "matcher-group")
+    public void listenOrderBook(String message) {
+        // Forward order book to WebSocket clients
+        messagingTemplate.convertAndSend("/topic/orderbook", message);
+    }
 }
