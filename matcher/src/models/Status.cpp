9939c0b3f732e9f6033d557cb3310fa8f92c1c93
JaspreetG <99363580+JaspreetG@users.noreply.github.com>
2025-06-08 18:10:18 +0530

    fixed market order matching

--- matcher/src/models/Status.cpp
+++ matcher/src/models/Status.cpp
@@ -3,15 +3,18 @@
 namespace models
 {
 
-    Status::Status(std::string orderId, std::string userId, int quantity, std::chrono::system_clock::time_point ts)
+    Status::Status(std::string orderId, std::string userId, std::string side, int quantity, std::chrono::system_clock::time_point ts)
     {
         orderId_ = orderId;
         userId_ = userId;
+        side_ = side;
         quantity_ = quantity;
         ts_ = ts;
     }
     const std::string &Status::orderId() const { return orderId_; }
     const std::string &Status::userId() const { return userId_; }
+    const std::string &Status::side() const { return side_; }
     int Status::quantity() const { return quantity_; }
+    std::chrono::system_clock::time_point Status::timestamp() const { return ts_; }
 
 } // namespace models
