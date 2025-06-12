fe8ea2d9f60d89a14606ae2f2ae1c20231fe40d1
JaspreetG <99363580+JaspreetG@users.noreply.github.com>
2025-06-12 15:54:16 +0530

    push

--- frontend/src/lib/ConnectWebSocket.ts
+++ frontend/src/lib/ConnectWebSocket.ts
@@ -1,32 +1,33 @@
 // ws.ts (or a custom hook like useWebSocket.ts)
-import { Client } from '@stomp/stompjs';
-import SockJS from 'sockjs-client';
+import { Client } from "@stomp/stompjs";
+import SockJS from "sockjs-client";
 
-const socketUrl = 'http://localhost:8082/ws'; // Use trade-service port
+// const socketUrl = 'http://localhost:8082/ws'; // Use trade-service port
+const socketUrl = "http://trade-service:8082/ws"; // Use trade-service port
 
 let stompClient: Client | null = null;
 
 export const connectWebSocket = (onLtpUpdate: (ltp: string) => void) => {
-    stompClient = new Client({
-        webSocketFactory: () => new SockJS(socketUrl),
-        reconnectDelay: 5000,
-        onConnect: () => {
-            console.log('Connected to WebSocket');
+  stompClient = new Client({
+    webSocketFactory: () => new SockJS(socketUrl),
+    reconnectDelay: 5000,
+    onConnect: () => {
+      console.log("Connected to WebSocket");
 
-            // Subscribe to LTP topic
-            stompClient?.subscribe('/topic/ltp', (message) => {
-                const ltp = message.body;
-                onLtpUpdate(ltp);
-            });
-        },
-        onStompError: (frame) => {
-            console.error('Broker reported error: ', frame.headers['message']);
-        },
-    });
+      // Subscribe to LTP topic
+      stompClient?.subscribe("/topic/ltp", (message) => {
+        const ltp = message.body;
+        onLtpUpdate(ltp);
+      });
+    },
+    onStompError: (frame) => {
+      console.error("Broker reported error: ", frame.headers["message"]);
+    },
+  });
 
-    stompClient.activate();
+  stompClient.activate();
 };
 
 export const disconnectWebSocket = () => {
-    stompClient?.deactivate();
+  stompClient?.deactivate();
 };
